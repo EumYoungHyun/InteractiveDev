541df5114259caf4824a529f93b14d412c313918
EumYoungHyun <opwer032@naver.com>
2021-05-09 05:59:13 +0900

    feat: init sprite sheet

---
+++ 02_spriteSheet/index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#canvas"),
+  readOut = document.querySelector("#read-out"),
+  context = canvas.getContext("2d"),
+  spritesheet = new Image();
+
+function windowToCanvas(canvas, x, y) {
+  let bbox = canvas.getBoundingClientRect();
+}
---
+++ amI.js
@@ -0,0 +1,4 @@
+while (true) {
+  practice();
+  if (dreamsCameTrue()) break;
+}
