441f73ddae6a388dfdf154cbc530523e6a6a0e1c
Eum <opwer032@naver.com>
2021-05-11 09:20:17 +0900

    feat: Check instanve of canvas and context

--- 03_drawShapes/index.js
+++ 03_drawShapes/index.js
@@ -1,4 +1,13 @@
-const canvas = document.querySelector("#canvas");
-const context = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
 
-console.log(context);
+    // check width and height of the canvas
+    console.log("dimensions", canvas.width, canvas.height);
+
+    // verify canvas DOM node
+    console.log(canvas instanceof HTMLCanvasElement, canvas.HTMLC);
+
+    // verify rendering context interface
+    console.log(ctx instanceof CanvasRenderingContext2D);
+});
