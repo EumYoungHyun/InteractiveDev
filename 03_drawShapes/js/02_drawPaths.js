69ff14e4606182ad4de756ab335fea22b3d2f384
Eum <opwer032@naver.com>
2021-05-17 09:46:41 +0900

    feat: draw rectangle with moveTo and lineTo

--- 03_drawShapes/js/02_drawPaths.js
+++ 03_drawShapes/js/02_drawPaths.js
@@ -2,4 +2,39 @@ document.addEventListener("DOMContentLoaded", function () {
     /** @type {HTMLCanvasElement} */
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
+    /**
+     *  The CanvasRenderingContext2D interface gives us
+     *  ctx.moveTo() and ctx.lineTo() methods for this.
+     *
+     *  Think about drawing a straight path on a paper with a pencil.
+     *  first, i need to put the pencil on the paper at some location (x,y).
+     *  then you need to draw a line from that point to the next location (x,y).
+     *  if you need to draw a new line,
+     *  i need to repeat these two operations.
+     */
+
+    // Before you draw any shape on the canvas,
+    // i need to call the beginPath() method. Once i have drawn paths,
+    // i can use the fill() and stroke() methods.
+    // The fill() operation can only be performed on closed paths
+    // such as rectangles or circles.You can use the ctx.closePath() method
+    // to close the path instead of using the lineTo() method
+    // to draw a line to the first point.
+
+    // If path is not closed, the fill() operation will implicitly
+    // call the closePath() method to close the path before performing
+    // the filling operation.
+    // This however doesn't happen with the stroke() operation
+
+    /*----------------------*/
+
+    // draw rectangle
+    ctx.fillStyle = "green"; // fill color
+    ctx.beginPath();
+    ctx.moveTo(100, 100); // move pen to x:100, y:100
+    ctx.lineTo(300, 100); // draw upside line
+    ctx.lineTo(300, 300); // draw right side line
+    ctx.lineTo(100, 300); // draw down side line
+    ctx.lineTo(100, 100); // draw left side line
+    ctx.fill(); // path is close so, i can use fill method
 });
