b1640665116c0294920eb6a73ff9695f0be458bc
Eum <opwer032@naver.com>
2021-05-18 10:30:04 +0900

    feat: Init javascript code

---
+++ 04_rubberbanding/index.js
@@ -0,0 +1,4 @@
+const canvas = document.querySelector("#canvas"),
+    context = canvas.getContext("2d"),
+    rubberbandDiv = document.querySelector("rubberbandDiv"),
+    resetButton = document.querySelector("resetButton");
