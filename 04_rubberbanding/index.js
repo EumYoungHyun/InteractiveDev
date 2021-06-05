188981c7ab6ba883b44df30d1182d49b4553c54d
EumYoungHyun <opwer032@naver.com>
2021-06-05 16:35:48 +0900

    feat: Define functions for rubberband

--- 04_rubberbanding/index.js
+++ 04_rubberbanding/index.js
@@ -1,4 +1,84 @@
-const canvas = document.querySelector("#canvas"),
+(() => {
+  const canvas = document.querySelector("#canvas"),
     context = canvas.getContext("2d"),
     rubberbandDiv = document.querySelector("rubberbandDiv"),
-    resetButton = document.querySelector("resetButton");
+    resetButton = document.querySelector("resetButton"),
+    image = new Image(),
+    mousedown = {},
+    rubberbandRectangle = {},
+    dragging = false;
+
+  function rubberbandStart(x, y) {
+    mousedown.x = x;
+    mousedown.y = y;
+
+    rubberbandRectangle.left = mousedown.x;
+    rubberbandRectangle.top = mousedown.y;
+
+    moveRubberbandDiv();
+    showRubberbandDiv();
+
+    dragging = true;
+  }
+
+  function rubberbandStretch(x, y) {
+    rubberbandRectangle.left = x < mousedown.x ? x : mousedown.x;
+    rubberbandRectangle.top = y < mousedown.y ? y : mousedown.y;
+    (rubberbandRectangle.width = Math.abs(x - mousedown.x)),
+      (rybberband.height = Math.abs((y = mousedown.y)));
+    moveRubberbandDiv();
+    resizeRubberbandDiv();
+  }
+
+  function rubberbandEnd() {
+    let bbox = canvas.getBoundingClientRect();
+
+    try {
+      context.drawImage(
+        canvas,
+        rubberbandRectangle.left - bbox.left,
+        rubberbandRectangle.top - bbox.top,
+        rubberbandRectangle.width,
+        rubberbandRectangle.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    } catch (e) {
+      // 캔버스를 벗어난 곳에서 마우스를 떼어도,
+      // 에러 메시지를 보내지 않는다
+    }
+
+    resetRubberbandRectangle();
+
+    rubberbandDiv.style.width = 0;
+    rubberbandDiv.style.height = 0;
+
+    hideRubberbandDiv();
+
+    dragging = false;
+  }
+
+  function moveRubberbandDiv() {
+    rubberbandDiv.style.left = rubberbandRectangle.left + "px";
+    rubberbandDiv.style.top = rubberbandRectangle.top + "px";
+  }
+
+  function resizeRubberbandDiv() {
+    rubberbandDiv.style.width = rubberbandRectangle.width + "px";
+    rubberbandDiv.style.height = rubberbandRectangle.height + "px";
+  }
+
+  function showRubberbandDiv() {
+    rubberbandDiv.style.display = "inline";
+  }
+
+  function hideRubberbandDiv() {
+    rubberbandDiv.style.display = "none";
+  }
+
+  function resetRubberbandRectangle() {
+    rubberbandRectangle = { top: 0, left: 0, width: 0, height: 0 };
+  }
+})();
