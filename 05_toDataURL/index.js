2cbdf7f3178a2e6fbfd30efa9dd012158c2a933d
EumYoungHyun <opwer032@naver.com>
2021-06-06 13:01:07 +0900

    feat: Complete canvas clock

--- 05_toDataURL/index.js
+++ 05_toDataURL/index.js
@@ -55,11 +55,27 @@
     context.fill();
   }
 
+  function drawNumerals() {
+    const numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let angle = 0,
+      numeralWidth = 0;
+
+    numerals.forEach((numeral) => {
+      angle = (Math.PI / 6) * (numeral - 3);
+      numeralWidth = context.measureText(numeral).width;
+      context.fillText(
+        numeral,
+        canvas.width / 2 + Math.cos(angle) * HAND_RADIUS - numeralWidth / 2,
+        canvas.height / 2 + Math.sin(angle) * HAND_RADIUS + FONT_HEIGHT / 3
+      );
+    });
+  }
   function drawClock() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawCircle();
     drawHands();
     drawCenter();
+    drawNumerals();
   }
 
   loop = setInterval(drawClock, 10);
