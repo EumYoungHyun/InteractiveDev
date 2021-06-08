709d3b9f116df93815b6e12caafb2aaf88b64042
EumYoungHyun <opwer032@naver.com>
2021-06-08 22:25:16 +0900

    feat: Add rectangles

--- 06_rectangle/index.js
+++ 06_rectangle/index.js
@@ -1,7 +1,7 @@
 (() => {
   const canvas = document.querySelector("#canvas"),
     context = canvas.getContext("2d");
-  context.lineJoin = "rount";
+  context.lineJoin = "round";
   context.lineWidth = 30;
 
   context.font = "24px Helvetica";
@@ -10,6 +10,17 @@
   context.strokeRect(75, 100, 200, 200);
   context.fillRect(325, 100, 200, 200);
 
+  context.lineJoin = "round";
+  context.lineWidth = 30;
+
+  context.font = "24px Helvetica";
+  context.fillText("Click anywhere to erase", 175, 600);
+
+  context.strokeStyle = "goldenrod";
+  context.fillStyle = "rgba(0, 0, 255, 0.5)";
+
+  context.strokeRect(75, 500, 200, 200);
+  context.fillRect(325, 500, 200, 200);
   context.canvas.onmousedown = function (e) {
     context.clearRect(0, 0, canvas.width, canvas.height);
   };
