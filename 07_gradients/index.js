3ddd343191f6fc5088e01cca9ca6cfa7cb96e903
EumYoungHyun <opwer032@naver.com>
2021-06-11 04:36:04 +0900

    fix: Change function rect to fillRect

--- 07_gradients/index.js
+++ 07_gradients/index.js
@@ -1,14 +1,23 @@
 (() => {
   const canvas = document.querySelector("#canvas"),
-    context = canvas.getContext("2d"),
-    gradient = context.createLinearGradient(0, 0, canvas.width, 0);
+    context = canvas.getContext("2d");
+  // let gradient = context.createLinearGradient(0, 0, canvas.width, 0);
+  // let gradient = context.createLinearGradient(0, 0, 0, canvas.height);
+  let gradient = context.createLinearGradient(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
 
+  console.log(gradient);
   gradient.addColorStop(0, "blue");
   gradient.addColorStop(0.25, "white");
   gradient.addColorStop(0.5, "purple");
   gradient.addColorStop(0.75, "red");
-  gradient.addColorStop(0.1, "yellow");
+  gradient.addColorStop(1, "yellow");
 
+  console.log(gradient);
   context.fillStyle = gradient;
-  context.rect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 })();
