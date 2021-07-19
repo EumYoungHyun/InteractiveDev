c65935c87a063b4b97599de83f6e3100a9e2a734
EumYoungHyun <opwer032@naver.com>
2021-07-19 23:26:09 +0900

    style: Change animation timing

--- 10_pageLoadingEndInteraction/index.js
+++ 10_pageLoadingEndInteraction/index.js
@@ -1,5 +1,8 @@
 (() => {
   const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-  tl.to(".text", { y: "0%", duration: 2, stagger: 1 });
-  tl.to(".slider", { y: "-100%", duration: 1.7 });
+  tl.to(".text", { y: "0%", duration: 1, stagger: 0.6 });
+  tl.to(".slider", { y: "-100%", duration: 1.1, delay: 1 });
+  tl.to(".intro", { y: "-100%", duration: 0.7 }, "-=1");
+  tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+  tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 })();
