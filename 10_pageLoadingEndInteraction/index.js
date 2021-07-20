1632df7d18f3dfa16ad43b8f4867be21e812d3ad
Eum <opwer032@naver.com>
2021-07-20 10:06:55 +0900

    style: Change disappear timing

--- 10_pageLoadingEndInteraction/index.js
+++ 10_pageLoadingEndInteraction/index.js
@@ -1,8 +1,8 @@
 (() => {
-  const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-  tl.to(".text", { y: "0%", duration: 1, stagger: 0.6 });
-  tl.to(".slider", { y: "-100%", duration: 1.1, delay: 1 });
-  tl.to(".intro", { y: "-100%", duration: 0.7 }, "-=1");
-  tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-  tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to(".text", { y: "0%", duration: 1, stagger: 0.6 });
+    tl.to(".slider", { y: "-100%", duration: 1.1, delay: 1 });
+    tl.to(".intro", { y: "-100%", duration: 0.6 }, "-=1.1");
+    tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 })();
