95c25758a83f91dd7093da15bf27e6523aabd578
Eum <opwer032@naver.com>
2021-07-21 09:34:57 +0900

    feat: Practice image control

--- 11_imagePixelControl/canvas1.js
+++ 11_imagePixelControl/canvas1.js
@@ -9,18 +9,19 @@
     image1.src = "image1.jpg";
 
     let lineHeight = 0;
-    let direction = 5;
+    let direction = 30;
     image1.addEventListener("load", () => {
         context.drawImage(image1, 0, 0, canvas.width, canvas.height);
     });
+
     function animate() {
         requestAnimationFrame(animate);
         lineHeight += direction;
-        if (lineHeight >= 3200) {
-            direction = -5;
+        if (lineHeight >= 3190) {
+            direction = -20;
         }
-        if (lineHeight <= 0) {
-            direction = 5;
+        if (lineHeight <= 10) {
+            direction = 20;
         }
         context.drawImage(image1, 0, 0, canvas.width, canvas.height);
         const scannedImage = context.getImageData(
@@ -38,6 +39,10 @@
                 scannedData[i] = averageColorValue;
                 scannedData[i + 1] = averageColorValue;
                 scannedData[i + 2] = averageColorValue;
+            } else {
+                scannedData[i] = 255 - scannedData[i];
+                scannedData[i + 1] = 255 - scannedData[i + 1];
+                scannedData[i + 2] = 255 - scannedData[i + 2];
             }
         }
         scannedImage.data = scannedData;
