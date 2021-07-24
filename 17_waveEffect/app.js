162ffb89d0112175a1f4c2394b423e77ae200e1a
EumYoungHyun <opwer032@naver.com>
2021-07-24 13:57:41 +0900

    feat: Build image to canvas

--- 17_waveEffect/app.js
+++ 17_waveEffect/app.js
@@ -5,7 +5,47 @@ class App {
     this.context = this.canvas.getContext("2d");
 
     this.pixelRatio = window.devicePixelRatio >= 1.5 ? 2 : 1;
-    console.log(window.devicePixelRatio);
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    this.isLoaded = false;
+    this.imagePosition = {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+    };
+
+    this.image = new Image();
+    this.image.src = "./dream_house.png";
+    this.image.onload = () => {
+      this.isLoaded = true;
+      this.drawImage();
+    };
+  }
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.context.scale(this.pixelRatio, this.pixelRatio);
+
+    if (this.isLoaded) {
+      this.drawImage();
+    }
+  }
+
+  drawImage() {
+    const stageRatio = this.stageWidth / this.stageHeight;
+    const imageRatio = this.image.width / this.image.height;
+    this.context.drawImage(
+      this.image,
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
   }
 }
 
