94f2f1d6add7f56b5e55abf9162023ffe795ac77
EumYoungHyun <opwer032@naver.com>
2021-07-25 09:09:12 +0900

    feat: Add draw image

--- 17_waveEffect/app.js
+++ 17_waveEffect/app.js
@@ -39,12 +39,36 @@ class App {
   drawImage() {
     const stageRatio = this.stageWidth / this.stageHeight;
     const imageRatio = this.image.width / this.image.height;
+
+    this.imagePosition.width = this.stageWidth;
+    this.imagePosition.height = this.stageHeight;
+
+    if (imageRatio > stageRatio) {
+      this.imagePosition.width = Math.round(
+        this.image.width * (this.stageHeight / this.image.height)
+      );
+      this.imagePosition.x = Math.round(
+        (this.stageWidth - this.imagePosition.width) / 2
+      );
+    } else {
+      this.imagePosition.height = Math.round(
+        this.image.height * (this.stageWidth / this.image.width)
+      );
+      this.imagePosition.y = Math.round(
+        (this.stageHeight - this.imagePosition.height) / 2
+      );
+    }
+
     this.context.drawImage(
       this.image,
       0,
       0,
-      this.canvas.width,
-      this.canvas.height
+      this.image.width,
+      this.image.height,
+      this.imagePosition.x,
+      this.imagePosition.y,
+      this.imagePosition.width,
+      this.imagePosition.height
     );
   }
 }
