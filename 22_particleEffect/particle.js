43b044184fcbb500e7837033c44077e3cf550292
Eumericano <opwer032@naver.com>
2021-09-23 00:07:32 +0900

    fix: Fix errors by this

--- 22_particleEffect/particle.js
+++ 22_particleEffect/particle.js
@@ -9,10 +9,11 @@ export default class Particle {
 
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
+    this.mouse = mouse;
   }
 
   draw() {
-    ctx.beginPath();
+    this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
     this.ctx.fillStyle = "#8C5523";
     this.ctx.fill();
@@ -28,20 +29,26 @@ export default class Particle {
 
     // Check circle collision
     // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    let dx = mouse.x - this.x;
-    let dy = mouse.y - this.y;
+    let dx = this.mouse.x - this.x;
+    let dy = this.mouse.y - this.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < mouse.radius + this.size) {
-      if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+    if (distance < this.mouse.radius + this.size) {
+      if (
+        this.mouse.x < this.x &&
+        this.x < this.canvas.width - this.size * 10
+      ) {
         this.x += 10;
       }
-      if (mouse.x > this.x && this.x > this.size * 10) {
+      if (this.mouse.x > this.x && this.x > this.size * 10) {
         this.x -= 10;
       }
-      if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+      if (
+        this.mouse.y < this.y &&
+        this.y < this.canvas.height - this.size * 10
+      ) {
         this.y += 10;
       }
-      if (mouse.y > this.y && this.y > this.size * 10) {
+      if (this.mouse.y > this.y && this.y > this.size * 10) {
         this.y -= 10;
       }
     }
