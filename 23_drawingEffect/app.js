cb78aa580225cd484a6315a06fa27e9622ce1016
Eumericano <opwer032@naver.com>
2021-10-03 06:35:47 +0900

    feat: Draw a image

--- 23_drawingEffect/app.js
+++ 23_drawingEffect/app.js
@@ -3,10 +3,26 @@ class App {
     this.canvas = document.createElement("canvas");
     this.imageCanvas = document.createElement("canvas");
     //이미지를 뒤에 띄어놓고 그림만 복사해올 예정
-    this.imageCanvas.classList.add("image_canvas");
+
     document.body.appendChild(this.canvas);
-    document.body.appendChild(this.imageCanvas);
+
     this.ctx = this.canvas.getContext("2d");
+
+    const image = new Image();
+    image.src = "image.jpg";
+    image.onload = () => {
+      this.imageCanvas.classList.add("image_canvas");
+      document.body.appendChild(this.imageCanvas);
+      this.imageCtx = this.imageCanvas.getContext("2d");
+
+      this.imageCtx.drawImage(
+        image,
+        0,
+        0,
+        this.imageCanvas.width,
+        this.imageCanvas.height
+      );
+    };
 
     this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
