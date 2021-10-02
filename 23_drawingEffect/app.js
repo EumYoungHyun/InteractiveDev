a86c155f53ac4232c5b2173b7c25ff2476fb4843
Eum Young Hyun <opwer032@naver.com>
2021-10-02 21:53:46 +0900

    feat: Init canvas and image canvas

--- 23_drawingEffect/app.js
+++ 23_drawingEffect/app.js
@@ -1,1 +1,28 @@
-console.log("init");
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.imageCanvas = document.createElement("canvas");
+    //이미지를 뒤에 띄어놓고 그림만 복사해올 예정
+    this.imageCanvas.classList.add("image_canvas");
+    document.body.appendChild(this.canvas);
+    document.body.appendChild(this.imageCanvas);
+    this.ctx = this.canvas.getContext("2d");
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    this.thumbs = [];
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
