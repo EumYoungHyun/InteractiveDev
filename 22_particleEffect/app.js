6187e210a73acf55904ddb7849f67d99485efe8b
Eum Young Hyun <opwer032@naver.com>
2021-10-02 19:58:56 +0900

    feat: Add mouse event

--- 22_particleEffect/app.js
+++ 22_particleEffect/app.js
@@ -4,15 +4,16 @@ const ctx = canvas.getContext("2d");
 window.addEventListener("resize", resize);
 window.addEventListener("load", init);
 
+const pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
 function resize() {
-  const pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
   const stageWidth = document.body.clientWidth;
   const stageHeight = document.body.clientHeight;
 
   canvas.width = stageWidth * pixelRatio;
   canvas.height = stageHeight * pixelRatio;
 
-  mouse.radius = (canvas.height / 80) * (canvas.width / 80);
+  mouse.radius = (canvas.height / 100) * (canvas.width / 100);
 }
 
 let particlesArray;
@@ -20,67 +21,66 @@ let particlesArray;
 let mouse = {
   x: null,
   y: null,
-  radius: (canvas.height / 80) * (canvas.width / 80),
+  radius: (canvas.height / 160) * (canvas.width / 160),
 };
 
 window.addEventListener("mousemove", (e) => {
-  mouse.x = e.x;
-  mouse.y = e.y;
+  mouse.x = e.x * pixelRatio;
+  mouse.y = e.y * pixelRatio;
 });
 
-function init() {
-  class Particle {
-    constructor(x, y, directionX, directionY, size, color) {
-      this.x = x;
-      this.y = y;
-      this.directionX = directionX;
-      this.directionY = directionY;
-      this.size = size;
-      this.color = color;
-    }
-
-    draw() {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-      ctx.fillStyle = this.color;
-      ctx.fill();
-    }
-
-    update() {
-      if (this.x > canvas.width || this.x < 0) {
-        this.directionX = -this.directionX;
-      }
-      if (this.y > canvas.height || this.y < 0) {
-        this.directionY = -this.directionY;
-      }
-
-      // Check circle collision
-      // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-      let dx = mouse.x - this.x;
-      let dy = mouse.y - this.y;
-      let distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < mouse.radius + this.size) {
-        if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
-          this.x += 10;
-        }
-        if (mouse.x > this.x && this.x > this.size * 10) {
-          this.x -= 10;
-        }
-        if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-          this.y += 10;
-        }
-        if (mouse.y > this.y && this.y > this.size * 10) {
-          this.y -= 10;
-        }
-      }
-
-      this.x += this.directionX;
-      this.y += this.directionY;
-      this.draw();
-    }
+class Particle {
+  constructor(x, y, directionX, directionY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
   }
 
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX;
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY;
+    }
+
+    // Check circle collision
+    // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < mouse.radius + this.size) {
+      console.log(distance, mouse.x, mouse.y);
+      if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+        this.x += 10;
+      }
+      if (mouse.x > this.x && this.x > this.size * 10) {
+        this.x -= 10;
+      }
+      if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+        this.y += 10;
+      }
+      if (mouse.y > this.y && this.y > this.size * 10) {
+        this.y -= 10;
+      }
+    }
+    this.x += this.directionX;
+    this.y += this.directionY;
+    this.draw();
+  }
+}
+
+function init() {
   resize();
   particlesArray = [];
   let numberOfParticles = (canvas.width * canvas.height) / 9000;
